6a12e410738bf4f6afecb466c56723c58c31f3c6
emrakul <n.a.savelyev@gmail.com>
2023-03-31 23:13:07 +0400

    fix app.py

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 processor = Processor('./mobilenetv2.onnx')
 
-@app.route('/process')
+@app.route('/process', methods=['GET', 'POST'])
 def transcribe():
-    blobs = json.loads(request.data)
-    bytes_image1, bytes_image2 = blobs['image1'], blobs['image2']
-    image1 = base64.b64decode(bytes_image1.encode('ascii'))
-    image2 = base64.b64decode(bytes_image2.encode('ascii'))
+    blobs = request.get_json()
+    bytes_image1, bytes_image2 = blobs['left'], blobs['right']
+    image1 = base64.b64decode(bytes_image1)
+    image2 = base64.b64decode(bytes_image2)
     result = processor.compare(image1, image2)
-    return jsonify({"result" : result })
+    
+    return jsonify({"result" : str(result) })
